6d324cfa27edb45947ebccc7ed0d1d9b52e6d730
Michael Ackah <mackah666@yahoo.co.uk>
2016-04-10 15:48:24 +0100

    Initial spike check, had to make some changes to the original tutorial asboth ElasticType and ElasticProperty have changed with Nest 2.0 realease.

---
+++ ConsoleNest/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNest
+{
+    [ElasticsearchType(IdProperty = "Id", Name = "blog_post")]
+    public class BlogPost
+    {
+        [String(Name = "_id", Index = FieldIndexOption.NotAnalyzed)]
+        public Guid? Id { get; set; }
+
+        [String(Name = "title", Index = FieldIndexOption.Analyzed)]
+        public string Title { get; set; }
+
+        [String(Name = "body", Index = FieldIndexOption.Analyzed)]
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: '{0}', Title: '{1}', Body: '{2}'", Id, Title, Body);
+        }
+    }
+}
---
+++ ConsoleNest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var local = new Uri("http://localhost:9200");
+            var settings = new Nest.ConnectionSettings(local);
+            var elastic = new Nest.ElasticClient(settings);
+
+            var res = elastic.ClusterHealth();
+
+
+            Console.WriteLine(res.ClusterName);
+
+            Console.ReadLine();
+        }
+    }
+}
