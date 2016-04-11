59468aeb4d6edb6070a309569f446dfe046b97ef
mackah666 <mackah666@yahoo.co.uk>
2016-04-11 10:49:26 +0100

    This is now working correctly. Field property attributes have changed.

---
+++ ConsoleNest/NeoTweet.cs
@@ -0,0 +1,15 @@
+﻿using Nest;
+using System;
+
+namespace ConsoleNest
+{
+    internal class NeoTweet
+    {
+        [Number(Name = "Id", Index = NonStringIndexOption.No)]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime PostDate { get; set; }
+        [String(Name = "User", Index = FieldIndexOption.Analyzed)]
+        public string User { get; set; }
+    }
+}
--- ConsoleNest/Program.cs
+++ ConsoleNest/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Nest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,37 +11,65 @@ namespace ConsoleNest
     {
         static void Main(string[] args)
         {
-            var local = new Uri("http://localhost:9200");
-            var settings = new Nest.ConnectionSettings(local);
-            var elastic = new Nest.ElasticClient(settings);
+            //var local = new Uri("http://localhost:9200");
+            //var settings = new Nest.ConnectionSettings(local);
+            //var elastic = new Nest.ElasticClient(settings);
 
-            // time to create 
+            //// time to create 
 
-            var res = elastic.ClusterHealth();
-
-
-            Console.WriteLine(res.ClusterName);
+            //var res = elastic.ClusterHealth();
 
 
-            var blogPost = new BlogPost
+            //Console.WriteLine(res.ClusterName);
+
+            //for (int i = 0; i < 20; i++)
+            //{
+
+            //    var blogPost = new BlogPost
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Title = "Another blog post",
+            //        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie a arcu eget blandit!"
+            //    };
+
+            //    var blogId = blogPost.Id;
+
+
+            //    var res1 = elastic.Index(blogPost, 
+            //        b=>b.Index("index_blog"));
+
+            //    var resGet = elastic.Search<BlogPost>(s=>s
+            //    .From(0)
+            //    .Size(10)
+            //    .Query(q=>q.Term(p=>p.Id, "blogId")));
+
+            //    Console.WriteLine(resGet); 
+            //}
+
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+            int rollingId = 1;
+
+            for (int i = 0; i < 10; i++)
             {
-                Id = Guid.NewGuid(),
-                Title = "Second blog post",
-                Body = "This is very long blog post!"
-            };
 
-            var blogId = blogPost.Id;
+                var tweet = new NeoTweet
+                {
+                    Id = rollingId,
+                    User = "kimchy",
+                    PostDate = DateTime.Now,
+                    Message = "Trying out NEST, so far so good?"
+                };
+
+                var response = client.Index(tweet, idx => idx.Index("neoindex")); //or specify index via settings.DefaultIndex("mytweetindex");
+                Console.WriteLine(response);
+                ++rollingId;
+                System.Threading.Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
 
 
-            var res1 = elastic.Index(blogPost, p => p
-                 .Index("my_first_index")
-                 .Id(blogPost.Id.ToString())
-                 .Refresh());
 
-            var resGet = elastic.Get<BlogPost>();
-
-            Console.WriteLine(resGet.Source);
-            //elastic.Get<BlogPost>
 
             Console.ReadLine();
         }
